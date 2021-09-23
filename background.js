72f2a3e399e41f98ec901f38f28824e6cffc4290
Ryan <ryanlee250@gmail.com>
2021-09-22 21:00:05 -0700

    add todo list

--- background.js
+++ background.js
@@ -3,4 +3,20 @@ let color = '#3aa757';
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+  getLocation();
 });
+
+
+// var x = document.getElementById("demo");
+function getLocation() {
+  console.log(navigator);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  return "Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude;
+}
--- popup.js
+++ popup.js
@@ -1,12 +1,18 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
 
+
+// storage.sync represents storage for all users
+// storage.local represents storage for the current user
+// chrome.storage holds all properties of the extension
 chrome.storage.sync.get("color", ({ color }) => {
+    // Changes background color of extension's little window
     changeColor.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
+    // Gets current tab
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
